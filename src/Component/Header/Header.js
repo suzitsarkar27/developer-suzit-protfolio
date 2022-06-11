ca639f952b1048902a684a52b1514d48377a6bc1
suzitsarkar27 <96899998+suzitsarkar27@users.noreply.github.com>
2022-06-11 18:33:03 +0600

    start-navbar

--- src/Component/Header/Header.js
+++ src/Component/Header/Header.js
@@ -1,6 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Header = () => {
+     // fixed Header
+ window.addEventListener("scroll", function () {
+    const header = document.querySelector(".header")
+    header.classList.toggle("active", window.scrollY > 100)
+  })
+  // Toogle Menu
+  const [Mobile, setMobile] = useState(false)
     return (
         <div>
             <h2>This is heaser</h2>
